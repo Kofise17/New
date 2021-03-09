ec1bb6bc27f6d341ab7ef33653639220d66a6cbd
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2021-03-09 11:46:13 +0100

    Start to the project

---
+++ js/functional1.js
@@ -0,0 +1,18 @@
+function Login(){
+    document.getElementById("Saved").innerText = "Your credentials have been saved.";
+  }
+  
+  function Register(){
+    document.getElementById("Saved").innerText = "Your credentials have been saved.";
+  }
+  
+  function Erase(){
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
+    document.getElementById('name').value = "";
+    document.getElementById('firstname').value = "";
+    document.getElementById('addressline').value = "";
+    document.getElementById('domicile').value = "";
+    document.getElementById("Saved").innerHTML = "";
+    document.getElementById('name').focus();
+  }
